4c9c0eb616930105ff2198a8e4605ef83c037706
Thiago Memória Barroso <71512175+thiago-memoria@users.noreply.github.com>
2022-06-02 17:38:04 -0300

    Update playlists.js

--- models/playlists.js
+++ models/playlists.js
@@ -2,12 +2,10 @@ cosnt mongoose = require('mongoose')
 
 const Playlists = mongoose.model('Playlist', {
     
-    
     "id": number,
     "imagem": String,
     "musicas": [],
     
-
 })
 
 module.exports = Playlists
